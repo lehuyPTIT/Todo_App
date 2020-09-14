cfed2df63e8af94c05d991a72b2b6d7a6c8f3ca4
nghiatd16 <nghiatd.proptit@gmail.com>
2020-09-15 03:47:06 +0700

    update

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain, dialog} = require('electron')
 const path = require('path')
+const DataAccessObject = require('./src/dao');
 
+const dao = new DataAccessObject("./data.json");
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
     }
   })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
+  mainWindow.on("close", () => {
+    mainWindow.webContents.send('store-data', 'test');
+  })
 }
+
+ipcMain.on('store-data', (event, data) => {
+  dao.data = data;
+  dao.writeData();
+})
+
+ipcMain.on("did-finish-load", async e => {
+  console.log("did-finist-load called");
+  let data = await dao.readData();
+  e.sender.send("onload-data", data);
+})
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -29,6 +45,8 @@ app.whenReady().then(() => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    
+    
   })
 })
 
@@ -36,8 +54,10 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
+
   if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
--- ui/script.js
+++ ui/script.js
@@ -1,12 +1,30 @@
-if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", ready);
-  } else {
-    ready();
+const electron = require("electron");
+const ipc = electron.ipcRenderer;
+
+const saveData = () => {
+  let rsp_json = {
+    listItem : dataListItems,
+    doing : dataDoingList,
+    completed : dataCompletedList
   }
-  
-  function ready() {
-    var date = new Date();
-    var days = [
+  ipc.send('store-data', rsp_json);
+}
+
+ipc.on('onload-data', (event, arg) => {
+  render(arg);
+})
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+var dataListItems = [];
+var dataDoingList = [];
+var dataCompletedList = [];
+function ready() {
+  var date = new Date();
+  var days = [
       "Sunday",
       "Monday",
       "Tuesday",
@@ -14,158 +32,235 @@ if (document.readyState === "loading") {
       "Thursday",
       "Friday",
       "Saturday"
-    ];
-    document.getElementsByClassName("day")[0].innerHTML = date.getDate();
-    document.getElementsByClassName(
+  ];
+  document.getElementsByClassName("day")[0].innerHTML = date.getDate();
+  document.getElementsByClassName(
       "month"
-    )[0].innerHTML = date.toDateString().substr(4, 3);
-    document.getElementsByClassName("year")[0].innerHTML = date.getFullYear();
-    document.getElementsByClassName("day-in-week")[0].innerHTML =
+  )[0].innerHTML = date.toDateString().substr(4, 3);
+  document.getElementsByClassName("year")[0].innerHTML = date.getFullYear();
+  document.getElementsByClassName("day-in-week")[0].innerHTML =
       days[date.getDay()];
-  
-    const addBtn = document.getElementById("add-button");
-    addBtn.addEventListener("click", addItem);
-  
-    const resetProgress = document.getElementById("reset-progress");
-    resetProgress.addEventListener("click", reset);
-  
-    const coll = document.getElementsByClassName("action-type");
-    for (let i = 0; i < coll.length; i++) {
+
+  const addBtn = document.getElementById("add-button");
+  addBtn.addEventListener("click", addItem);
+
+  const resetProgress = document.getElementById("reset-progress");
+  resetProgress.addEventListener("click", reset);
+
+  const coll = document.getElementsByClassName("action-type");
+  for (let i = 0; i < coll.length; i++) {
       coll[i].addEventListener("click", collapsible);
-    }
   }
-  
-  function addItem() {
-    const newItem = document.getElementsByClassName("item-name")[0];
-    const listAllItem = document.getElementById("list-item");
-    if (newItem.value !== "") {
-      let data = item(newItem.value);
-      listAllItem.append(data);
-      for (let i = 0; i < listAllItem.childElementCount; i++) {
-        const deleteBtn = listAllItem.getElementsByClassName("delete")[i];
-        deleteBtn.addEventListener("click", deleteItem);
-  
-        const doLaterBtn = listAllItem.getElementsByClassName("do-later")[i];
-        doLaterBtn.addEventListener("click", addDoLater);
-  
-        const completedBtn = listAllItem.getElementsByClassName("completed")[i];
-        completedBtn.addEventListener("click", addCompleted);
-      }
+  ipc.send('did-finish-load');
+}
+
+function render(jsonData) {
+  const items = document.getElementById("list-item");
+  const doingItems = document.getElementById("doing-list");
+  const completedItems = document.getElementById("completed-list");
+
+  dataListItems = jsonData.listItem;
+  dataDoingList = jsonData.doing;
+  dataCompletedList = jsonData.completed;
+  showListItem(dataListItems, items);
+  showListItem(dataDoingList, doingItems);
+  showListItem(dataCompletedList, completedItems);
+  updateProgress();
+}
+
+function reRender() {
+  const items = document.getElementById("list-item");
+  const doingItems = document.getElementById("doing-list");
+  const completedItems = document.getElementById("completed-list");
+  showListItem(dataListItems, items);
+  showListItem(dataDoingList, doingItems);
+  showListItem(dataCompletedList, completedItems);
+  updateProgress();
+}
+
+function showListItem(listItem, items) {
+  items.innerHTML = "";
+  for (let i = 0; i < listItem.length; i++) {
+      item(listItem[i], items);
+  }
+}
+
+function addItem() {
+  const newItem = document.getElementsByClassName("item-name")[0];
+  const items = document.getElementById("list-item");
+  if (newItem.value !== "") {
+      item(newItem.value, items);
+      dataListItems.push(newItem.value);
       newItem.value = "";
       updateProgress();
-    }
+      saveData();
   }
-  
-  function item(name) {
-    let itemRow = document.createElement("div");
-    itemRow.classList.add("item");
-    itemRow.classList.add("border-radius");
-    itemRow.classList.add("flex-between-center");
-    let itemContent = `
-      <div class="name">${name}</div>
-      <div class="action flex-between-center">
-        <div class="delete pointer"></div>
-        <div class="do-later pointer"></div>
-        <div class="completed pointer"></div>
-      </div>
-    `;
-    itemRow.innerHTML = itemContent;
-    return itemRow;
-  }
-  
-  function deleteItem(e) {
-    let item = e.target.parentNode;
-    item.closest(".item").remove();
-    updateProgress();
-  }
-  
-  function addDoLater(e) {
-    const completedIcon = `<div class="completed pointer"></div>`;
-    const doLaterList = document.getElementById("do-later-list");
-    let item = e.target.parentNode.closest(".item");
-    let cloneItem = item.cloneNode(true);
-    item.remove();
-    cloneItem.getElementsByClassName("do-later")[0].remove();
-    if (cloneItem.getElementsByClassName("completed").length === 0) {
-      cloneItem
-        .getElementsByClassName("action")[0]
-        .insertAdjacentHTML("beforeend", completedIcon);
-    }
-    doLaterList.append(cloneItem);
-    doLaterList.parentNode.querySelector(".action-type").classList.add("active");
-    doLaterList.style.maxHeight = doLaterList.scrollHeight + "px";
-    for (let i = 0; i < doLaterList.childElementCount; i++) {
-      const deleteBtn = doLaterList.getElementsByClassName("delete")[i];
+}
+
+function item(name, items) {
+  let itemRow = document.createElement("div");
+  itemRow.classList.add("item");
+  itemRow.classList.add("border-radius");
+  itemRow.classList.add("flex-between-center");
+  let itemContent = `
+    <div class="name">${name}</div>
+    <div class="action flex-between-center">
+      <div class="delete pointer"></div>
+      <div class="doing pointer"></div>
+      <div class="completed pointer"></div>
+    </div>
+  `;
+  itemRow.innerHTML = itemContent;
+  items.append(itemRow);
+  for (let i = 0; i < items.childElementCount; i++) {
+      const deleteBtn = items.getElementsByClassName("delete")[i];
       deleteBtn.addEventListener("click", deleteItem);
-  
-      const completedBtn = doLaterList.getElementsByClassName("completed")[i];
+
+      const doLaterBtn = items.getElementsByClassName("doing")[i];
+      doLaterBtn.addEventListener("click", addDoing);
+
+      const completedBtn = items.getElementsByClassName("completed")[i];
       completedBtn.addEventListener("click", addCompleted);
-    }
-    updateProgress();
   }
+}
+
+function removeArrayElementByValue(arrayName, value) {
+  let array = null;
+  if(arrayName === "list-item"){
+    array = dataListItems;
+  }
+  else if (arrayName === "doing-list"){
+    array = dataDoingList;
+  }
+  else{
+    array = dataCompletedList;
+  }
+  const index = array.indexOf(value);
+  if (index > -1) {
+    array.splice(index, 1);
+  }
+}
+
+function deleteItem(e) {
+  let item = e.target.parentNode;
+  let taskName = item.closest(".item").textContent.trim();
+  let taskType = item.closest(".item").parentNode.id;
+  removeArrayElementByValue(taskType, taskName);
+  item.closest(".item").remove();
+  updateProgress();
+  saveData();
+}
+
+function addDoing(e) {
+  const completedIcon = `<div class="completed pointer"></div>`;
+  const doingList = document.getElementById("doing-list");
+  let item = e.target.parentNode.closest(".item");
+  let cloneItem = item.cloneNode(true);
+  // Remove from data List
+  let taskName = item.textContent.trim();
+  let taskType = item.parentNode.id;
+  removeArrayElementByValue(taskType, taskName);
+  item.remove();
   
-  function addCompleted(e) {
-    const doLaterIcon = `<div class="do-later pointer"></div>`;
-    const completedList = document.getElementById("completed-list");
-    let item = e.target.parentNode.closest(".item");
-    let cloneItem = item.cloneNode(true);
-    item.remove();
-    cloneItem.getElementsByClassName("completed")[0].remove();
-    if (cloneItem.getElementsByClassName("do-later").length === 0) {
+  // cloneItem.getElementsByClassName("doing")[0].remove();
+  if (cloneItem.getElementsByClassName("completed").length === 0) {
       cloneItem
-        .getElementsByClassName("action")[0]
-        .insertAdjacentHTML("beforeend", doLaterIcon);
-    }
-    completedList.append(cloneItem);
-    completedList.parentNode
+          .getElementsByClassName("action")[0]
+          .insertAdjacentHTML("beforeend", completedIcon);
+  }
+  doingList.append(cloneItem);
+  dataDoingList.push(taskName);
+  doingList.parentNode.querySelector(".action-type").classList.add("active");
+  doingList.style.maxHeight = doingList.scrollHeight + "px";
+  for (let i = 0; i < doingList.childElementCount; i++) {
+      const deleteBtn = doingList.getElementsByClassName("delete")[i];
+      deleteBtn.addEventListener("click", deleteItem);
+
+      const doLaterBtn = doingList.getElementsByClassName("doing")[i];
+      doLaterBtn.addEventListener("click", addDoing);
+
+      const completedBtn = doingList.getElementsByClassName("completed")[i];
+      completedBtn.addEventListener("click", addCompleted);
+  }
+  updateProgress();
+  saveData();
+}
+
+function addCompleted(e) {
+  const doingIcon = `<div class="doing pointer"></div>`;
+  const completedList = document.getElementById("completed-list");
+  let item = e.target.parentNode.closest(".item");
+  let cloneItem = item.cloneNode(true);
+  // Remove from data List
+  let taskName = item.textContent.trim();
+  let taskType = item.parentNode.id;
+  removeArrayElementByValue(taskType, taskName);
+  item.remove();
+  // cloneItem.getElementsByClassName("completed")[0].remove();
+  if (cloneItem.getElementsByClassName("doing").length === 0) {
+      cloneItem
+          .getElementsByClassName("action")[0]
+          .insertAdjacentHTML("beforeend", doingIcon);
+  }
+  dataCompletedList.push(taskName);
+  completedList.append(cloneItem);
+  completedList.parentNode
       .querySelector(".action-type")
       .classList.add("active");
-    completedList.style.maxHeight = completedList.scrollHeight + "px";
-    for (let i = 0; i < completedList.childElementCount; i++) {
+  completedList.style.maxHeight = completedList.scrollHeight + "px";
+  for (let i = 0; i < completedList.childElementCount; i++) {
       const deleteBtn = completedList.getElementsByClassName("delete")[i];
       deleteBtn.addEventListener("click", deleteItem);
-  
-      const doLaterBtn = completedList.getElementsByClassName("do-later")[i];
-      doLaterBtn.addEventListener("click", addDoLater);
-    }
-    updateProgress();
+
+      const doLaterBtn = completedList.getElementsByClassName("doing")[i];
+      doLaterBtn.addEventListener("click", addDoing);
+
+      const completedBtn = completedList.getElementsByClassName("completed")[i];
+      completedBtn.addEventListener("click", addCompleted);
   }
-  
-  function reset() {
-    let item = document.getElementsByClassName("item");
-    while (item[0]) {
-      item[0].parentNode.removeChild(item[0]);
-      updateProgress();
-    }
-  }
-  
-  function collapsible(e) {
-    const thisItem = e.target;
-    thisItem.classList.toggle("active");
-    let content = thisItem.nextElementSibling;
-    if (content.style.maxHeight) {
+  updateProgress();
+  saveData();
+}
+
+function reset() {
+    dataCompletedList.forEach(element => {
+      dataListItems.push(element);
+    });
+    dataCompletedList = [];
+    dataDoingList.forEach(element => {
+      dataListItems.push(element);
+    })
+    dataDoingList = [];
+    reRender();
+    saveData();
+}
+
+function collapsible(e) {
+  const thisItem = e.target;
+  thisItem.classList.toggle("active");
+  let content = thisItem.nextElementSibling;
+  if (content.style.maxHeight) {
       content.style.maxHeight = null;
-    } else {
+  } else {
       content.style.maxHeight = content.scrollHeight + "px";
-    }
   }
-  
-  function updateProgress() {
-    const countItems = document.getElementsByClassName("item").length;
-    const countDoLaterItems = document.getElementById("do-later-list")
+}
+
+function updateProgress() {
+  const countItems = document.getElementsByClassName("item").length;
+  const countDoLaterItems = document.getElementById("doing-list")
       .childElementCount;
-    const countCompletedItems = document.getElementById("completed-list")
+  const countCompletedItems = document.getElementById("completed-list")
       .childElementCount;
-    const completedPercent = (countCompletedItems / countItems) * 100;
-    const doLaterPercent = (countDoLaterItems / countItems) * 100;
-    const completedProgress = document.getElementById("completed-progress");
-    const doLaterProgress = document.getElementById("do-later-progress");
-    if (countItems > 0) {
+  const completedPercent = (countCompletedItems / countItems) * 100;
+  const doLaterPercent = (countDoLaterItems / countItems) * 100;
+  const completedProgress = document.getElementById("completed-progress");
+  const doLaterProgress = document.getElementById("doing-progress");
+  if (countItems > 0) {
       completedProgress.style.width = completedPercent + "%";
       doLaterProgress.style.width = doLaterPercent + "%";
-    } else {
+  } else {
       completedProgress.style.width = "0%";
       doLaterProgress.style.width = "0%";
-    }
   }
-  
+}
